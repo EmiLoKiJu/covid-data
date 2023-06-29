83984f1fd79b4d1e788907cd4884b84401f4010d
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-06-29 09:12:24 -0800

    Add credits

---
+++ src/components/credits.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+
+const CreditsForThis = () => {
+  const background = useSelector((store) => store.cases.color);
+  return (
+    <div className={`credits bc${background}`}>
+      <div>
+        Design: Nelson Sakwa on&nbsp;
+        <a href="https://www.behance.net/gallery/31579789/Ballhead-App-(Free-PSDs)" target="_blank" rel="noreferrer">Behance</a>
+      </div>
+      <div>
+        Maps images:&nbsp;
+        <a href="https://vemaps.com/" target="_blank" rel="noreferrer">Vemaps</a>
+      </div>
+      <div>
+        Some of the icons:&nbsp;
+        <a href="https://www.iconpacks.net/" target="_blank" rel="noreferrer">IconPacks</a>
+      </div>
+    </div>
+  );
+};
+
+export default CreditsForThis;

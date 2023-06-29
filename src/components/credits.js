2e360330b4110d82f4484a1abac144c48b41f82b
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-06-28 18:08:53 -0800

    Add some style to the credits page

--- src/components/credits.js
+++ src/components/credits.js
@@ -4,9 +4,18 @@ const Credits = () => {
   const background = useSelector((store) => store.cases.color);
   return (
     <div className={`credits bc${background}`}>
-      <div>maps images: https://vemaps.com/</div>
-      <div>some of the icons: https://www.iconpacks.net/</div>
-      <div>Design: https://www.behance.net/gallery/31579789/Ballhead-App-(Free-PSDs)</div>
+      <div>
+        Design: Nelson Sakwa on&nbsp;
+        <a href="https://www.behance.net/gallery/31579789/Ballhead-App-(Free-PSDs)" target="_blank" rel="noreferrer">Behance</a>
+      </div>
+      <div>
+        Maps images:&nbsp;
+        <a href="https://vemaps.com/" target="_blank" rel="noreferrer">Vemaps</a>
+      </div>
+      <div>
+        Some of the icons:&nbsp;
+        <a href="https://www.iconpacks.net/" target="_blank" rel="noreferrer">IconPacks</a>
+      </div>
     </div>
   );
 };
